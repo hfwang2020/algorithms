6bd5cbf02818e50eb9bb93cf99d54c3f70f43953
hfwang_mac <hfwang1111@foxmail.com>
2020-11-12 09:32:43 +0800

    add reference

---
+++ src/ch1/_3/Node.java
@@ -0,0 +1,6 @@
+package ch1._3;
+
+public class Node<Item> {
+    Item item;
+    Node next;
+}
--- src/priv/hfwang/ch1/_3/Node.java
+++
@@ -1,6 +0,0 @@
-package priv.hfwang.ch1._3;
-
-public class Node<Item> {
-    Item item;
-    Node next;
-}
